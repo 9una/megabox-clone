012d4f7f377bee44e829a8d3c3f34b59890b76a5
9una <guna_9@naver.com>
2022-01-31 16:35:00 +0900

    main page finish
    add
    + 링크이미지 사용 -> 이미지 다운로드 후 사용
    + header gnb > button - call menu
    + headern gnb:hover lnb visible
    + slide-bn - slide js (이제 혼자서도 안보고 짬! 대박 ㅎㅋ)

---
+++ js/index.js
@@ -0,0 +1,50 @@
+function slideBanner(){
+    let curPos = 0;
+    const slide = document.querySelector('#slide-bn .slide'),
+        item = slide.querySelectorAll('li'),
+        pagination = document.querySelector('#slide-bn .pagination'),
+        pagiDot = pagination.querySelector('.dot'),
+        num = pagination.querySelector('.num'),
+        all = pagination.querySelector('.all'),
+        prevBtn = pagination.querySelector('.prev-btn'),
+        nextBtn = pagination.querySelector('.next-btn');
+
+    all.innerText = item.length;
+    num.innerText = curPos + 1;
+
+    function removeItem(){
+        let j = 0;
+        while(j < item.length){
+            item[j++].classList.remove('active');
+        }
+    }
+
+    function prev(){
+        if(curPos > 0){
+            curPos -= 1;
+        }else{
+            curPos = item.length - 1;
+        }
+        removeItem();
+        item[curPos].classList.add('active');
+        pagiDot.style.transform = `translateX(${100 * curPos}%)`;
+        num.innerText = curPos + 1;
+    }
+
+    function next(){
+        if(curPos < item.length - 1){
+            curPos += 1;
+        }else {
+            curPos = 0;
+        }
+        removeItem();
+        item[curPos].classList.add('active');
+        pagiDot.style.transform = `translateX(${100 * curPos}%)`;
+        num.innerText = curPos + 1;
+    }
+
+    prevBtn.addEventListener('click', prev);
+    nextBtn.addEventListener('click', next);
+    
+}
+slideBanner();
